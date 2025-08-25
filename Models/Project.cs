b63d766d424312fc91d07fdaff65d669444ce13f
niloy <chowdhuryniloy819@gmail.com>
2025-08-25 23:02:05 +0600

    Initial commit with project setup

---
+++ Models/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YourProjectNamespace.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }  // Primary key
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }  // Link to project or GitHub
+        public string ImageUrl { get; set; }  // Optional image path
+    }
+}
