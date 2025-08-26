c3a28e0efb7aaa4a624db6217ccf1d9e9d8e015f
niloy <chowdhuryniloy819@gmail.com>
2025-08-26 07:04:22 +0600

    Connected my project with SSMS through connection string

---
+++ Migrations/202508260044442_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿namespace Personal_Portfolio2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ContactMessages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Email = c.String(),
+                        Message = c.String(),
+                        DateSent = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Educations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Degree = c.String(nullable: false),
+                        Institute = c.String(nullable: false),
+                        Year = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Projects",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false),
+                        Description = c.String(),
+                        Url = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Skills",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Level = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Skills");
+            DropTable("dbo.Projects");
+            DropTable("dbo.Educations");
+            DropTable("dbo.ContactMessages");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Personal_Portfolio2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Personal_Portfolio2.Models.PortfolioContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Personal_Portfolio2.Models.PortfolioContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
