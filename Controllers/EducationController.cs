92ba2970e693045e7a469510fdcee0e388b6fe4f
niloy <chowdhuryniloy819@gmail.com>
2025-08-28 00:18:49 +0600

    designed the Education page(the css needs to be update further).

---
+++ Controllers/EducationController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Personal_Portfolio2.Models;
+
+namespace Personal_Portfolio2.Controllers
+{
+    public class EducationController : Controller
+    {
+        private PortfolioContext db = new PortfolioContext();
+
+        public ActionResult Index()
+        {
+            var educations = db.Educations.OrderByDescending(e => e.Id).ToList();
+            return View(educations);
+        }
+    }
+}
