759a8bea816aa598a0f33fae5ebd3ec7cb250138
niloy <chowdhuryniloy819@gmail.com>
2025-08-29 18:07:30 +0600

    completed the designing of the create page and solved the database issue in inserting the project.added download cv button on the hero section.

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
-namespace Personal_Portfolio2.Models 
+namespace Personal_Portfolio2.Models
 {
     public class Project
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string Title { get; set; }
         public string Description { get; set; }
         public string Url { get; set; }
